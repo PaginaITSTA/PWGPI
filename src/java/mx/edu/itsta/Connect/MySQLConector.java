aa6d215ab742401b121b54cd6db7bb1e8efc2e4c
Clair <acheron_1405@hotmail.com>
2017-10-31 21:20:10 -0600

    Solo unas modificaciones

--- src/java/mx/edu/itsta/Connect/MySQLConector.java
+++ src/java/mx/edu/itsta/Connect/MySQLConector.java
@@ -1,20 +1,48 @@
 package mx.edu.itsta.Connect;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Clair
  */
 public class MySQLConector {
 
-    public MySQLConector() {
-    }
-
-    public void ConectarASQL() {
-
-    }
+    private String url = "jdbc:mysql://localhost/feedback?";
+    private String user = "root";
+    private String password = "erick";
+    private Connection con = null;
 
     //Aquí debe de regresar la conección
-    public void getConnection() {
-
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error de Driver");
+        } catch (SQLException e2) {
+            System.out.println("Un error con la base de datos");
+        }
+        return this.con;
     }
+
+    public void cerrarConnection() throws SQLException {
+        if (this.con != null) {
+            this.con.close();
+        }
+    }
+
+    /*       
+    
+    public void cerrarConnection() throws SQLException{
+        if (this.con!=null) {
+            this.con.close();
+        }
+    }
+
+    public conexion(){}
+    
+    
+     */
 }
