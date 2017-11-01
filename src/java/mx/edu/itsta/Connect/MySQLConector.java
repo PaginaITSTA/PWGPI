fecd848961b96cf50c4e9d7c6fbdd742d707fd5a
Yakiis <jomeha148@gmail.com>
2017-10-31 23:09:23 -0600

    archivo conexion

--- src/java/mx/edu/itsta/Connect/MySQLConector.java
+++ src/java/mx/edu/itsta/Connect/MySQLConector.java
@@ -1,20 +1,45 @@
 package mx.edu.itsta.Connect;
 
-/**
- *
- * @author Clair
- */
-public class MySQLConector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-    public MySQLConector() {
+public class MySQLConector{
+   static String bd = "bd_materia";
+    static String login = "root";
+    static String password = "";
+    static String port ="3306";
+    static String url = "jdbc:mysql://localhost/:"+port+"/"+bd;
+    
+    Connection conn = null;
+    
+     public MySQLConector(String login, String password) {
+        this.login = "root";
+        this.password = "";
     }
-
-    public void ConectarASQL() {
-
+    
+    public void ConectaraSQL() {
+     System.out.println("conecta");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //obtenemos la conexión
+            conn = DriverManager.getConnection(url,login,password);
+            
+            if (conn != null) {
+                System.out.println("[MYSQL] Conexión a base de datos ");
+            }
+            }catch (SQLException | ClassNotFoundException e) {
+            System.out.println("[MYSQL] Conexión a la base de datos " + bd + ".....FAILED");
+            System.out.println(e.toString());
+        }
     }
+   
 
-    //Aquí debe de regresar la conección
-    public void getConnection() {
-
+    public Connection getConnection() {
+      return conn;
+    }
+    
+    public void desconectar() {
+        conn = null;
     }
 }
