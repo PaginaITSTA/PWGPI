a1acfe18b24e3b629ab50df0e55bb01366caecbb
Clair <acheron_1405@hotmail.com>
2017-11-13 00:53:10 -0600

    Separados procesos de coneccion y creado acceso a la bd

--- src/java/mx/edu/itsta/Connect/Conexion.java
+++ src/java/mx/edu/itsta/Connect/Conexion.java
@@ -1,79 +1,108 @@
 package mx.edu.itsta.Connect;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import mx.edu.itsta.DTO.DTOPersona;
+
 /**
  *
  * @author Yaquiii
  */
-public class Conexion{
+public class Conexion {
 //    static String bd = "bd_materia";
+
     static String login = "root";
     static String password = "";
 //    static String port ="3306";
-//    static String url = "jdbc:mysql://localhost:3306/bd_materia";
-    
+    static String url = "jdbc:mysql://localhost:3306/bd_materia";
+
     Connection conn = null;
     Statement sql = null;
-    
-     public Conexion(String login, String password) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+    public Conexion(String login, String password) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         this.login = "root";
         this.password = "";
         this.ConectaraSQL();
     }
-    
-     /**
-     * Constructor que establece la conexión con el usuario por default (root o administrador)
+
+    /**
+     * Constructor que establece la conexión con el usuario por default (root o
+     * administrador)
      */
-    public Conexion() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
-        this.login = "root";
-        this.password = "";
+    
+    public Conexion() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Conexion.login = "root";
+        Conexion.password = "erick";
         this.ConectaraSQL();
     }
-    
-    public void ConectaraSQL()  {
-     System.out.println("conecta");
-    try{
-        Class.forName("com.mysql.jdbc.Driver");
-        conn = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_materia","root","");
-        sql = conn.createStatement();
-        System.out.print("conexion establecida" );
-    }catch(Exception e){
-        System.out.print("Error en la conexion" +e);
+
+    public void ConectaraSQL() {
+        System.out.println("conecta");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection(url, login, password);
+            sql = conn.createStatement();
+            System.out.print("conexion establecida");
+        } catch (Exception e) {
+            System.out.print("Error en la conexion" + e);
+        }
     }
-}
-    
-    
-    
-    
-   public Connection getConnection() {
+
+    public Connection getConnection() {
         return conn;
     }
-    
-   public void desconectar() {
-       try{
-       sql.close();
-       conn.close();
-       }catch(SQLException e){
-           System.out.println("Error mientras se cierra la conexión");
-       }
+
+    public void desconectar() {
+        try {
+            sql.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Error mientras se cierra la conexión");
+        }
 //        conn = null;
     }
 
-   public boolean ingreso_datos(DTOPersona persona){
-    boolean encontrado= false;
-    try{
-    ConectaraSQL();
-    int i=sql.executeUpdate("INSERT INTO Persona VALUES ('"+persona.getId_persona()+"','"+persona.getNombre()+"',"
-        +persona.getApellidoP()+","+persona.getApellidoM()+")");
-       if(i > 0){
-        encontrado =true;
-       }
-    } catch (Exception e){
-    e.getStackTrace();
+    public boolean ingreso_datos(DTOPersona persona) {
+        boolean encontrado = false;
+        try {
+            ConectaraSQL();
+            System.out.println("Nombre de persona es: " + persona.getNombre());
+            int i = sql.executeUpdate("INSERT INTO bd_materia.persona VALUES (null , '" + persona.getNombre() + "','"
+                    + persona.getApellidoP() + "' , '" + persona.getApellidoM() + "');");
+            encontrado = (i == 1) ? true : false;
+            //System.out.println("Encontrado = " + i);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return encontrado;
     }
-    return encontrado;
-    }
+    
+    /**
+     * 
+    *public static void main(String[] args) throws SQLException {
+    *    try {
+    *        //Solo se conecta            
+    *        Conexion con = new Conexion();
+    *        
+    *        DTOPersona per = new DTOPersona();
+    *        per.setId_persona(15);
+    *        per.setNombre("Usuario2");
+    *        per.setApellidoP("ApellidoP");
+    *        per.setApellidoM("ApellidoM");
+    *        
+    *        System.out.println("Resultado es: " + con.ingreso_datos(per));
+    *        
+    *    } catch (ClassNotFoundException ex) {
+    *        Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+    *    } catch (InstantiationException ex) {
+    *        Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+    *    } catch (IllegalAccessException ex) {
+    *        Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+    *    }
+    *}
+    */
 }
