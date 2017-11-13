29518961f5074058e881add26d2425d3d44482a8
Clair <acheron_1405@hotmail.com>
2017-11-13 00:52:32 -0600

    Creado Bussines de la PW

--- src/java/mx/edu/itsta/Bussines/Bussines.java
+++ src/java/mx/edu/itsta/Bussines/Bussines.java
@@ -1,9 +1,17 @@
 package mx.edu.itsta.Bussines;
 
+import java.sql.SQLException;
+import mx.edu.itsta.DAO.DAOUsuario;
+import mx.edu.itsta.DTO.DTOLogin;
+
 /**
  *
  * @author Clair
  */
 public class Bussines {
-    
+
+    public int validUser(DTOLogin login) throws SQLException {
+        DAOUsuario ingreso = new DAOUsuario();
+        return ingreso.validarUsuario(login);
+    }
 }
